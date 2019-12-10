95ac63ff085954ae8a95d21dc065021a2c91c894
mradul_k_mobile-di <mradul@mobile-di.com>
2019-12-10 22:38:15 +0530

    prime number series upto n number

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -4,6 +4,22 @@ public class PrimeNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int upto = 100;
+		for (int i=2; i<upto ; i++) {
+			int count = 0;
+			for(int j=2; j<upto; j++) {
+				if(j<=i) {
+					if(i%j == 0) {
+						count++;
+						if(count >= 2) 
+							break;
+					}
+				}else break;
+			}
+			if(count == 1) {
+				System.out.print(i+" ");
+			}
+		}
 	}
 
 }
